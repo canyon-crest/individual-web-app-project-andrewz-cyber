6caec27031481d92a8efccc448b70b54b09a41f5
andrewz-cyber <andrewz112233@gmail.com>
2026-03-03 21:52:53 +0000

    Changes made to HTML and CSS

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// add javascript here
+document.addEventListener('DOMContentLoaded', (event) => {
+    const showImageButton = document.getElementById('But');
+    const MyImage = document.getElementById('Myimage');
+
+    showImageButton.addEventListener('click', () => {
+        // Toggles the hidden attribute (true/false)
+        MyImage.hidden = !MyImage.hidden;
+    });
+});
+
